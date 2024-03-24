fa387c4c2545257426eec132da8984e238fe05ae
iksin1706 <49130419+iksin1706@users.noreply.github.com>
2024-03-24 15:30:33 +0100

    fix: gallery overflow fix

--- src/sections/GallerySection/GallerySection.tsx
+++ src/sections/GallerySection/GallerySection.tsx
@@ -30,7 +30,7 @@ const GallerySection = () => {
     })
 
     return (
-        <section className={`py-32`}>
+        <section className={`py-32 overflow-hidden`}>
             <div className="w-full py-5 inline-flex overflow-hidden flex-nowrap photos -rotate-3 [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
                 <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
                     {images.map((item, idx) => (
