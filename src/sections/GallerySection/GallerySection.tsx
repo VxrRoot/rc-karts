c721356c5ebe83c5aa3c191c64f8b63850c9539b
iksin1706 <49130419+iksin1706@users.noreply.github.com>
2024-03-24 15:28:33 +0100

    fix: gallery key

--- src/sections/GallerySection/GallerySection.tsx
+++ src/sections/GallerySection/GallerySection.tsx
@@ -34,14 +34,14 @@ const GallerySection = () => {
             <div className="w-full py-5 inline-flex overflow-hidden flex-nowrap photos -rotate-3 [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
                 <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
                     {images.map((item, idx) => (
-                        <li className={`shadow-xl w-96 h-56`}>
+                        <li key={idx} className={`shadow-xl w-96 h-56`}>
                             <Image alt={'Photo'} src={item} className="object-cover w-full h-full" />
                         </li>
                     ))}
                 </ul>
                 <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
                     {images.map((item, idx) => (
-                        <li className={`shadow-xl w-96 h-56`}>
+                        <li key={idx} className={`shadow-xl w-96 h-56`}>
                             <Image alt={'Photo'} src={item} className="object-cover w-full h-full" />
                         </li>
                     ))}
