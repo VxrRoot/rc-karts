9a0e99eef111346358e62a44540a9f3179826d65
Dawid Slowik <dawidslowik379@gmail.com>
2024-04-14 22:34:14 +0200

    remove quote

--- src/sections/NewsSection/NewsSectionHomePage/elements/BigNews.tsx
+++ src/sections/NewsSection/NewsSectionHomePage/elements/BigNews.tsx
@@ -17,7 +17,7 @@ const BigNews = () => {
         <span>20/02/2024</span>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
